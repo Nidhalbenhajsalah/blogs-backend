7255c8d20d12113fefc1b8cfb9e31c88cd736fbd
Nidhalbenhajsalah <nidal86@gmail.com>
2025-02-05 14:26:26 +0100

    get and post apis

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const blogRoutes = require('./routes');
+
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use('/api/blogs', blogRoutes);
+
+mongoose.connect('mongodb://localhost:27017/blogs', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
+app.listen(5000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
