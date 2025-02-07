4d098ca8dafd0c7fb50f76fda7a4933438cf8440
Nidhalbenhajsalah <nidal86@gmail.com>
2025-02-07 09:37:50 +0100

    upvote qnd downvote

--- routes/blogsRoutes.js
+++ routes/blogsRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getBlogs, getBlog, postBlog } = require('../controllers/blogsController');
+const { getBlogs, getBlog, postBlog, upvoteBlog, downvoteBlog } = require('../controllers/blogsController');
 
 router.get('/', getBlogs);
 router.get('/:id', getBlog);
 router.post('/', postBlog);
+router.put('/upvote/:id', upvoteBlog);
+router.put('/downvote/:id', downvoteBlog);
 
 module.exports = router;
